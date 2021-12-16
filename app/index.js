747b7a34bac8af6564b7fee1781566aa259aed9d
AlexyPulivelil <alexypulivelil@gmail.com>
2021-12-16 20:00:33 +0530

    Sample commit

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, your server is working!')
+  res.send('Hello, your server is working in good condition!')
 })
 app.listen(5000, () => console.log('Server is up and running'));
